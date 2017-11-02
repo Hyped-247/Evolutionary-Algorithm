5abf446c36bba029bf788e9e3734ca37322a3e53
Emma Donelson <edonelson@Emmas-MacBook-Pro-2.local>
2017-11-02 11:39:27 -0700

    Basic functionality for Individual, Main, and Domain

--- src/Evolutionary/Main.java
+++ src/Evolutionary/Main.java
@@ -1,23 +1,52 @@
 package Evolutionary;
 
+import java.util.Collections;
 
 public class Main {
+    /**
+     * The method whoLives calculates the survivors based on a tournament selection algorithm
+     * @param Array<Individual> - an array of individuals, representing the population
+     * @return Array<Individual> - the individuals that have been chosen to survive
+     */
+    public Array<Individual> whoLives(Array<Individual> population){
+        // TODO
+    }
+    
+    // On the board we have crossNum being a parameter but that is not needed as a param
+    /**
+     * The method reproduce constructs an array of individuals representing the children of two individuals
+     * @param Individual - The first parent
+     * @param Individual - The second parent
+     */
+    public Array<Individual> reproduce(Individual indi1 , Individual indi2){
+        // TODO
+    }
+    
+    /**
+     * The method createInitPop creates an Array<Individual> that represents the population. These individuals
+     * are created randomly.
+     * @param popSize - the population size, as set in the Domain.java class
+     * @return Array<Individual> - the initial population representing the first generation of the test
+     */
+    public Array<Individual> createInitPop(int popSize){
+        // TODO
+    }
+    
     public static void main(String[] args) {
-        // create a population of 1000 Individuals
-        Population population1 = new Population(1000);
-
-        population1.survive(3, 10);
-        /*
-        System.out.println(population1.getIndividuals());
-        for (Individual x : population1.getIndividuals()){
-            System.out.println(x.getIndividual_id());
-            System.out.println(x.getGenetic_makeup_list());
-            System.out.println(x.getFitness_level());
+        // TODO
+        for(int i = 0 ; i < popSize-1 ; i++){
+            population[i].setFitness(domain.computeFitness(population[i]));
         }
-        */
-        for (int i = 0; i < population1.getSurvivesLinkedList().size(); i++) {
-            System.out.println(population1.getSurvivesLinkedList().get(i).getGenetic_makeup_string());
-        }
-
+//         Compute fitness and store
+//         Compute the standard diviation over fitness and store
+//         Select for survival
+//         Select for reproduction
+//         Mutate
+//         Create a population from these individuals
+//         Compute maximum fitness and store
+//         Compute minimum fitness and store
+//         Print this for every generation: genNum_k_avgFit_ft_stdv_f2_maxFit_f3_minFit_f4
+//         Restart
+        
     }
 }
