ed353a6820f4b5704410ab4cfe5084778e1cf706
Mohammad Mahjoub <mmahjoub@westmont.edu>
2017-11-02 11:32:53 -0700

    It is really to go!

---
+++ src/Evolutionary/Domain.java
@@ -0,0 +1,16 @@
+package Evolutionary;
+
+
+
+/**
+ * @author Mo
+ * @see Individual : in the Individual class is where everything is created.
+ *
+ **/
+class Domain extends AbstractDomain {
+
+    @Override
+    public double computeFitness(Individual i) {
+        return 0;
+    }
+}
---
+++ testing/Evolutionary/DomainTest.java
@@ -0,0 +1,16 @@
+package Evolutionary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mohammad on 11/2/17.
+ */
+public class DomainTest {
+    @Test
+    public void computeFitness() throws Exception {
+
+    }
+
+}
