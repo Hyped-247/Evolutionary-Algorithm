9c8cc3da4badc0f9d359af1b96832aee38c8b4c3
Mohammad Mahjoub <mmahjoub@westmont.edu>
2017-11-23 09:58:35 -0800

    some changes

--- src/Evolutionary/trt.java
+++ src/Evolutionary/trt.java
@@ -9,14 +9,6 @@ import java.util.ArrayList;
 public class trt {
     public static void main(String[] args) throws Exception {
 
-        ArrayList<Integer> allIndexes = new ArrayList<>();
-        allIndexes.add(0);
-        allIndexes.add(2);
-        allIndexes.add(4);
-        allIndexes.add(7);
-
-        String father = "0110100";
-        String mother = "1000110";
 
 
 
